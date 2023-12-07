486c6747bede4e28a784135a26294ab4939bfffb
imran122 <imransardar122@gmail.com>
2023-12-07 23:45:30 +0600

    fix server file

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ mongoose
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(); 
-const corsOptions ={
-  origin:'https://law-firm-system-client.vercel.app', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://law-firm-system-client.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use(cors());
 // routes attached with server
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", require("./routes/auth"));
